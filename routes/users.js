186f03ef6d63ef45f5aff35e1d47e3d23fcca59d
Shruthi_2103 <shruthichouhan2001@gmail.com>
2021-06-14 09:39:08 +0530

    Login Completed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+const userLib=require('../backend/lib/userLib.js')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login',function(req,res){
+  //var responseJson ={success:true,message:'Login Failed',user:null};
+  
+userLib.isUserValid(req.body, function(resultJson){
+  res.json(resultJson);
+  })
 });
 
 module.exports = router;
